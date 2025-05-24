bf0b4bafd5c5c212f11e0b25f702ef120ee7c0ab
Mahamahu22 <mahalakshmirajan07@gmail.com>
2025-05-24 17:13:20 +0530

    Linked time HTML and JS files

---
+++ FULL STACK/Day 10/time.js
@@ -0,0 +1,6 @@
+fetch('http://worldtimeapi.org/api/timezone/Asia/Kolkata')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('time').textContent = Current time in India: ${data.datetime};
+  })
+  .catch(error => console.error('Error fetching time:', error));
